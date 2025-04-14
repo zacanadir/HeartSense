176a1d02dc7a92717a45b5f6c1e3ead7ab86925d
zacanadir <zacanadir@gmail.com>
2025-04-14 09:52:16 -0400

    Add files via upload

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,18 +1,7 @@
 import pandas as pd
-import os
-
-import pandas as pd
-import numpy as np
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import  ColumnTransformer
-from sklearn.pipeline import Pipeline
-from sklearn.model_selection import train_test_split
-from sklearn.inspection import permutation_importance
-from sklearn.metrics import accuracy_score, roc_curve, precision_recall_curve, average_precision_score, roc_auc_score
-from sklearn.linear_model import LogisticRegression
-from sklearn.ensemble import RandomForestClassifier
-from tensorflow.keras import layers, Model, Input
-import tensorflow as tf
+import os
 
 
 
